62c761efb696f29d510d08d765f40d8705d7553b
Michael Ripley <zkxs00@gmail.com>
2017-09-10 03:56:25 -0500

    Reduced memory usage of program as much as I can under Java

--- runWarframeSprint.sh
+++ runWarframeSprint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 EXECUTABLE="$(ls -t1 target/*-jar-with-dependencies.jar | head -n1)" # get most recently modified jar file
 echo "Running $EXECUTABLE" # tell the user which version we are running
-java -jar "$EXECUTABLE" 2>&1 | tee -a WarframeSprint.log # run the program and append output to log
+java -Xms16m -Xmx16m -Xss4k -jar "$EXECUTABLE" 2>&1 | tee -a WarframeSprint.log # run the program and append output to log
